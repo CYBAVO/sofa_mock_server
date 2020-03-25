d154a21ef002247ffe3aa0d7d3ce0e6fcf71421c
JoeLee <joe@cybavo.com>
2020-03-25 12:09:44 +0800

    Change route to static route

--- routers/router.go
+++ routers/router.go
@@ -16,17 +16,22 @@ import (
 )
 
 func init() {
-	InitUpdateSRVNameSpace()
-}
+	beego.Router("/v1/mock/wallets/:wallet_id/apitoken", &controllers.OuterController{}, "POST:SetAPIToken")
+	beego.Router("/v1/mock/wallets/:wallet_id/addresses", &controllers.OuterController{}, "POST:CreateDepositWalletAddresses")
+	beego.Router("/v1/mock/wallets/:wallet_id/addresses", &controllers.OuterController{}, "GET:GetDepositWalletAddresses")
+	beego.Router("/v1/mock/wallets/:wallet_id/pooladdress", &controllers.OuterController{}, "GET:GetDepositWalletPoolAddresses")
+	beego.Router("/v1/mock/wallets/:wallet_id/callback/resend", &controllers.OuterController{}, "POST:CallbackResend")
+	beego.Router("/v1/mock/wallets/:wallet_id/withdraw", &controllers.OuterController{}, "POST:WithdrawTransactions")
+	beego.Router("/v1/mock/wallets/:wallet_id/sender/transactions/:order_id", &controllers.OuterController{}, "GET:GetWithdrawTransactionState")
+	beego.Router("/v1/mock/wallets/:wallet_id/apisecret", &controllers.OuterController{}, "GET:GetTxAPITokenStatus")
+	beego.Router("/v1/mock/wallets/:wallet_id/notifications", &controllers.OuterController{}, "GET:GetNotifications")
+	beego.Router("/v1/mock/wallets/:wallet_id/notifications/get_by_id", &controllers.OuterController{}, "POST:GetWalletNotificationsByID")
+	beego.Router("/v1/mock/wallets/:wallet_id/transactions", &controllers.OuterController{}, "GET:GetTransactionHistory")
+	beego.Router("/v1/mock/wallets/:wallet_id/blocks", &controllers.OuterController{}, "GET:GetWalletBlockInfo")
+	beego.Router("/v1/mock/wallets/:wallet_id/addresses/invalid-deposit", &controllers.OuterController{}, "GET:GetInvalidDepositAddresses")
+	beego.Router("/v1/mock/wallets/:wallet_id/info", &controllers.OuterController{}, "GET:GetWalletInfo")
+	beego.Router("/v1/mock/wallets/:wallet_id/addresses/verify", &controllers.OuterController{}, "POST:VerifyAddresses")
 
-func InitUpdateSRVNameSpace() {
-	ns :=
-		beego.NewNamespace("/v1",
-			beego.NSNamespace("/mock",
-				beego.NSInclude(
-					&controllers.OuterController{},
-				),
-			),
-		)
-	beego.AddNamespace(ns)
+	beego.Router("/v1/mock/wallets/callback", &controllers.OuterController{}, "POST:Callback")
+	beego.Router("/v1/mock/wallets/withdrawal/callback", &controllers.OuterController{}, "POST:WithdrawalCallback")
 }
